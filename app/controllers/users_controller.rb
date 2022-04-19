7b6f74cde98720b61e0e9414e415ac858de2ddbf
tranvanchilong <tranvanchilong@gmail.com>
2022-04-19 16:15:26 +0700

    exam_cart

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :selecting]
   before_action :correct_user, only: [:edit, :update, :show]
   
   def new
@@ -10,7 +10,14 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
-  def show; end
+  def show
+    if @user
+      @user_exams = @user.user_exams.includes(:exam) # fix N+1
+    else
+      flash[:danger] = "User not found"
+      redirect_to root_url
+    end
+  end
 
   def create
     @user = User.new user_params
@@ -28,6 +35,13 @@ class UsersController < ApplicationController
   def update
     flash[:success] = "Profile updated" if @user.update(user_params)
     render :edit
+  end
+
+  def selecting
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.selecting.paginate(page: params[:page])
+    render = 'show_follow'
   end
 
   private
@@ -54,4 +68,5 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
 end
